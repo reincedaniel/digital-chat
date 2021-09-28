ddae2c46e5eb89beaac3dcb6a5cfac4f98943736
Lourenço Daniel Sebastião Carlos <lourenco.carlos@ucall.ao>
2021-09-28 14:41:40 +0100

    feat: Create the Package

--- demo/vue.config.js
+++
@@ -1,7 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  lintOnSave: false,
-  productionSourceMap: isProd,
-  publicPath: isProd ? '/vue-beautiful-chat/' : '/'
-}
